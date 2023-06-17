dc3f201c04775e5ce85610a7d7950a7d2251eacb
Dongmin <pdm001125@naver.com>
2023-06-17 17:31:15 +0900

    특이한 정렬 추가 풀이
    특이한 정렬 추가 풀이

--- Algorithm/Programmers/src/LV.0/특이한 정렬.kt
+++ Algorithm/Programmers/src/LV.0/특이한 정렬.kt
@@ -1,10 +1,15 @@
 package LV.`0`
 
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
-    println(solution(intArrayOf(6,4,5,7,3), 5))
+    println(solution(intArrayOf(6,4,5,7,3), 5).toList().toString())
 }
+
+//fun solution(numlist: IntArray, n: Int) = numlist.sortedDescending().sortedBy { max(it, n) - min(it, n) }
+// 내가 푼 풀이는 아님. 하지만 원리는 같음. map을 사용하는게 아니라, 코틀린 내장함수를 활용해서 기준을 제시해주자.
 
 fun solution(numlist: IntArray, n: Int): IntArray {
     var answer: IntArray = intArrayOf()
