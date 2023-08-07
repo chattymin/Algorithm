b9ebf8866bfd2b608f5a8158a9869c25686d008e
Dongmin <pdm001125@naver.com>
2023-08-08 01:20:09 +0900

    programmers Lv.1 평균 구하기 solve
    programmers Lv.1 평균 구하기 solve

---
+++ Programmers/src/LV/1/평균 구하기.kt
@@ -0,0 +1,8 @@
+package LV.`1`
+
+fun main() {
+    println(solution(intArrayOf(1,2,3,4)))
+}
+
+//fun solution(arr: IntArray): Double = arr.sum()/arr.size.toDouble()
+fun solution(arr: IntArray): Double = arr.average()
