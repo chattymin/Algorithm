7a7647d41d2662e6fbe1d8aa86ec92459d119c32
Dongmin <pdm001125@naver.com>
2023-09-06 12:30:05 +0900

    Programmers Lv.1 직사각형 별찍기 Solve

---
+++ Programmers/src/LV/1/직사각형 별찍기.kt
@@ -0,0 +1,12 @@
+package LV.`1`
+
+fun main(args: Array<String>) {
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+
+    repeat(b){
+        repeat(a){
+            print("*")
+        }
+        println()
+    }
+}
