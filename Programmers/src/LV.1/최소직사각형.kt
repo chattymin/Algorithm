04bace69fdc69776c57d0e4ac2b54f9d42980e0e
Dongmin <pdm001125@naver.com>
2023-09-19 22:07:31 +0900

    Programmers Lv.1 최소직사각형 Solve

---
+++ Programmers/src/LV.1/최소직사각형.kt
@@ -0,0 +1,30 @@
+package LV.`1`
+
+import java.lang.Integer.max
+
+fun main() {
+    fun solution(sizes: Array<IntArray>): Int {
+        var maxNum = Int.MIN_VALUE
+        var minNum = Int.MIN_VALUE
+
+        sizes.forEach {
+            var maxLocal: Int
+            var minLocal: Int
+
+            if (it.first() > it.last()){
+                maxLocal = it.first()
+                minLocal = it.last()
+            }else {
+                maxLocal = it.last()
+                minLocal = it.first()
+            }
+            
+            maxNum = max(maxNum, maxLocal)
+            minNum = max(minNum, minLocal)
+        }
+
+        return maxNum * minNum
+    }
+
+    println(solution(arrayOf(intArrayOf(60, 50), intArrayOf(30, 70), intArrayOf(60, 30), intArrayOf(80, 40))))
+}
